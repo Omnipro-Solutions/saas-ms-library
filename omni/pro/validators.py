2d649373e1b53b2bb3e38a4832f31a949c41f820
Omni-Stiven <stiven.pinzon@omni.pro>
2023-04-12 12:32:15 -0500

    refactor: adding 'country_code' to ContextSchema validator

--- omni/pro/validators.py
+++ omni/pro/validators.py
@@ -4,6 +4,7 @@ from marshmallow import Schema, fields
 class ContextSchema(Schema):
     tenant = fields.String(required=True)
     user = fields.String(required=True)
+    country_code = fields.String(required=True)
 
 
 class BaseObjectSchema(Schema):
