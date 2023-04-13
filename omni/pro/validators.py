0444e0eb4d11c87cc8663abd880753989bdba1aa
Omni-Stiven <stiven.pinzon@omni.pro>
2023-04-13 10:09:37 -0500

    refactor: override load method in ContextSchema validator

--- omni/pro/validators.py
+++ omni/pro/validators.py
@@ -1,10 +1,24 @@
-from marshmallow import Schema, fields
+import typing
+
+from marshmallow import Schema, fields, types
 
 
 class ContextSchema(Schema):
     tenant = fields.String(required=True)
     user = fields.String(required=True)
-    country_code = fields.String(required=True)
+    countryCode = fields.String(required=True)
+
+    def load(
+        self,
+        data: (typing.Mapping[str, typing.Any] | typing.Iterable[typing.Mapping[str, typing.Any]]),
+        *,
+        many: bool | None = None,
+        partial: bool | types.StrSequenceOrSet | None = None,
+        unknown: str | None = None
+    ):
+        data = super().load(data, many=many, partial=partial, unknown=unknown)
+        data["country_code"] = data.pop("countryCode")
+        return data
 
 
 class BaseObjectSchema(Schema):
