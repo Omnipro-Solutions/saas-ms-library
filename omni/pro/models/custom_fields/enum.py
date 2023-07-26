cdef89f4251a42c00567a5c1e3be954e3b4ef8c9
Danna <danna.acero@omni.pro>
2023-07-26 15:44:23 +0000

    refactor: Refactor class EnumField in enum.py

--- omni/pro/models/custom_fields/enum.py
+++ omni/pro/models/custom_fields/enum.py
@@ -7,7 +7,16 @@ class EnumField(CharField):
         self.enum_type = enum_type
 
     def db_value(self, value):
-        return value.value if value else None
+        if isinstance(value, self.enum_type):
+            return value.value
+        elif isinstance(value, str):
+            try:
+                enum_value = self.enum_type(value)
+                return enum_value.value
+            except ValueError:
+                raise ValueError(f"Invalid value for enum {self.enum_type.__name__}: {value}")
+        else:
+            return value
 
     def python_value(self, value):
         return self.enum_type(value) if value else None
