1571cb79ea49d9e1bd67eb4c20e17beb39670d29
Edgar Elias Peña De La Torre <edgar.pena@omni.pro>
2023-07-28 00:03:35 -0500

    feat: Add functions to lib
    Add db property to BaseDocument
    Add territory matrix grpc files
    Add list convert dict to message
    Add normalize function with NFKD
    Change permission to "CAN_CHANGE_PASSWORD_USER"
    Add GRPClient to initial.py

--- omni/pro/initial.py
+++ omni/pro/initial.py
@@ -1,15 +1,15 @@
 import csv
 from pathlib import Path
 
-import grpc
-from google.protobuf import wrappers_pb2
 from omni.pro import redis, util
-from omni.pro.cloudmap import CloudMap
 from omni.pro.config import Config
 from omni.pro.database import DatabaseManager
 from omni.pro.logger import LoggerTraceback, configure_logger
-from omni.pro.models.common.ms import MicroService
-from omni.pro.protos.v1.users import user_pb2, user_pb2_grpc
+from omni.pro.models.common.ms import MicroService as MicroServiceDocument
+from omni.pro.protos.grpc_connector import Event, GRPClient
+from omni.pro.protos.v1.users import user_pb2
+from omni.pro.stack import ExitStackDocument
+from omni.pro.user.access import MicroService
 from omni.pro.validators import MicroServiceValidator
 
 logger = configure_logger(name=__name__)
@@ -44,19 +44,18 @@ class LoadData(object):
                 db_params = self.redis_manager.get_mongodb_config(Config.SERVICE_ID, tenant)
                 db_params["db"] = db_params.pop("name")
                 self.db_manager = DatabaseManager(**db_params)
-                with self.db_manager.get_connection():
-                    if not self.manifest:
-                        continue
-                    micro = self.load_manifest(context=context)
-                    self.load_data_micro(micro, context)
+                db_alias = f"{tenant}_{db_params['db']}"
+                if not self.manifest:
+                    continue
+                micro = self.load_manifest(context=context, db_alias=db_alias)
+                self.load_data_micro(micro, context, db_alias)
                 list_contexts.append(context)
         return list_contexts
 
     def create_user_admin(self, context, rc):
         values = self.load_data_dict(Path(__file__).parent / "data" / "models.user.csv")
-        user = UserStub()
-        response = user.create_users(context, values)
-        success = response.response_standard.status_code in range(200, 300)
+        user = UserChannel(context)
+        response, success = user.create_users(values)
         if success:
             rc.json().set(
                 context.get("tenant"), "$.user_admin", {"id": response.user.id, "username": response.user.username}
@@ -72,23 +71,26 @@ class LoadData(object):
             data = f.read()
         return eval(data)
 
-    def load_manifest(self, context: dict):
-        tenant = context.get("tenant")
-        filters = {"code": self.manifest.get("code"), "tenant_code": tenant}
-        micro: MicroService = self.db_manager.get_document(
-            db_name=None, tenant=tenant, document_class=MicroService, **filters
-        )
-        self.manifest["tenant_code"] = tenant
-        data_validated = MicroServiceValidator(self.base_app, micro.data if micro else []).load(
-            self.manifest | {"context": context}
-        )
-        if not micro:
-            micro = self.db_manager.create_document(db_name=None, document_class=MicroService, **data_validated)
-        else:
-            micro.data = data_validated.get("data")
-            micro.save()
-        self.microservice = micro
-        return micro
+    def load_manifest(self, context: dict, db_alias: str):
+        with ExitStackDocument(MicroServiceDocument.reference_list(), db_alias=db_alias):
+            tenant = context.get("tenant")
+            filters = {"code": self.manifest.get("code"), "tenant_code": tenant}
+            micro: MicroServiceDocument = self.db_manager.get_document(
+                db_name=None, tenant=tenant, document_class=MicroServiceDocument, **filters
+            )
+            self.manifest["tenant_code"] = tenant
+            data_validated = MicroServiceValidator(self.base_app, micro.data if micro else []).load(
+                self.manifest | {"context": context}
+            )
+            if not micro:
+                micro = self.db_manager.create_document(
+                    db_name=None, document_class=MicroServiceDocument, **data_validated
+                )
+            else:
+                micro.data = data_validated.get("data")
+                micro.save()
+            self.microservice = micro
+            return micro
 
     def load_data_dict(self, name_file):
         try:
@@ -102,7 +104,7 @@ class LoadData(object):
         except Exception as e:
             LoggerTraceback.error("An unexpected error has occurred", e, logger)
 
-    def load_data_micro(self, micro: MicroService, context: dict):
+    def load_data_micro(self, micro: MicroServiceDocument, context: dict, db_alias):
         tenant = context.get("tenant")
         for idx, file in enumerate(micro.data):
             if file.get("load"):
@@ -120,60 +122,57 @@ class LoadData(object):
                 logger.error(f"Class not found {model_str} in {ruta_modulo}")
                 continue
             load = False
-            for row in reader:
-                row = row | {"context": context}
-                self.db_manager.create_document(db_name=None, document_class=getattr(modulo, model_str), **row)
-                if not load:
-                    load = True
-            attr_data = {f"set__data__{idx}__load": load}
-            self.db_manager.update(micro, **attr_data)
-            if load:
-                logger.info(f"Load data {micro.code} - {tenant} - {file.get('path')}")
+            with ExitStackDocument(
+                (doc_class := getattr(modulo, model_str)).reference_list() + MicroServiceDocument.reference_list(),
+                db_alias=db_alias,
+            ):
+                for row in reader:
+                    row = row | {"context": context}
+                    self.db_manager.create_document(db_name=None, document_class=doc_class, **row)
+                    if not load:
+                        load = True
+                attr_data = {f"set__data__{idx}__load": load}
+                self.db_manager.update(micro, **attr_data)
+                if load:
+                    logger.info(f"Load data {micro.code} - {tenant} - {file.get('path')}")
 
 
-class UserStub(object):
-    def __init__(self):
-        cloud_map = CloudMap(service_name=Config.SERVICE_NAME_BALANCER)
-        response = cloud_map.discover_instances()
-        self.port = None
-        self.host = None
-        for instance in response:
-            if instance.get("InstanceId") == Config.SAAS_MS_USER:
-                self.host = instance.get("Attributes").get("host")
-                self.port = instance.get("Attributes").get("port")
-                break
+class UserChannel(object):
+    def __init__(self, context: dict) -> None:
+        self.context = context
+        self.service_id = MicroService.SAAS_MS_USER.value
+        self.user_module_grpc = "v1.users.user_pb2_grpc"
+        self.user_stub_classname = "UsersServiceStub"
+        self.user_module_pb2 = "v1.users.user_pb2"
 
-    def create_users(self, context: dict, list_value: list) -> user_pb2.UserCreateResponse:
-        credentials = grpc.ssl_channel_credentials()
-        with grpc.secure_channel(
-            f"{self.host}:{self.port}",
-            credentials,
-            options=[("grpc.ssl_target_name_override", "omni.pro")],
-        ) as channel:
-            stub = user_pb2_grpc.UsersServiceStub(channel)
-            response = user_pb2.UserCreateResponse()
-            for value in list_value:
-                context["user"] = context.get("user") or value.get("username")
-                response = self._create_user(context, value, stub)
-                if response.response_standard.status_code in range(200, 300):
-                    logger.info(f"User created {response.user.username}")
-                else:
-                    logger.error(f"User not created {context.get('user')}")
-            return response
-
-    def _create_user(self, context: dict, value: dict, stub) -> user_pb2.UserCreateResponse:
-        user = user_pb2.UserCreateRequest(
-            **{
-                "context": context,
-                "email": value.get("email"),
-                "email_confirm": value.get("email"),
-                "language": {"code": "01", "code_name": "CO"},
-                "name": value.get("name"),
-                "password": value.get("password"),
-                "password_confirm": value.get("password"),
-                "timezone": {"code": "01", "code_name": "CO"},
-                "username": value.get("username"),
-                "is_superuser": wrappers_pb2.BoolValue(value=util.parse_bool(value.get("is_superuser") or False)),
-            }
+        self.event = Event(
+            module_grpc=self.user_module_grpc,
+            stub_classname=self.user_stub_classname,
+            rpc_method="",
+            module_pb2=self.user_module_pb2,
+            request_class="",
+            params={},
         )
-        return stub.UserCreate(user)
+
+    def create_users(self, list_value):
+        response = user_pb2.UserCreateResponse(), False
+        for value in list_value:
+            self.context["user"] = self.context.get("user") or value.get("sub")
+            self.event.update(
+                rpc_method="UserCreate",
+                request_class="UserCreateRequest",
+                params={
+                    "context": self.context,
+                    "email": value.get("email"),
+                    "email_confirm": value.get("email"),
+                    "language": {"code": "01", "code_name": "CO"},
+                    "name": value.get("name"),
+                    "password": value.get("password"),
+                    "password_confirm": value.get("password"),
+                    "timezone": {"code": "01", "code_name": "CO"},
+                    "username": value.get("username"),
+                    "is_superuser": True,
+                },
+            )
+            response = GRPClient(service_id=self.service_id).call_rpc_fuction(self.event)
+        return response
