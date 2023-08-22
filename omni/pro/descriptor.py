6129ad2a3ea502cc1453ccb5b49830d2124793af
Edgar Elias Peña De La Torre <edgar.pena@omni.pro>
2023-08-22 15:55:43 -0500

    feat: Add refactor to SQLAlchemy

--- omni/pro/descriptor.py
+++ omni/pro/descriptor.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
 from mongoengine.fields import EmbeddedDocumentField, ReferenceField
-from peewee import ForeignKeyField
+from sqlalchemy import inspect
+from sqlalchemy.orm.relationships import RelationshipProperty
 
 
 class Descriptor(object):
@@ -37,23 +38,44 @@ class Descriptor(object):
         return description
 
     @staticmethod
-    def describe_peewee_model(model):
+    def describe_sqlalchemy_model(model):
+        mapper = inspect(model)
+
         description = {
             "model": model.__name__,
             "class_name": f"{model.__module__}.{model.__name__}",
             "name": model.__name__.lower(),
             "fields": [],
         }
-        for field_name, field in model._meta.fields.items():
-            field_info = {
-                "name": field_name,
-                "code": field_name,
-                "type": field.__class__.__name__,
-                "size": field.max_length if hasattr(field, "max_length") else None,
-                "required": not field.null,
-                "relation": field.rel_model.__name__ if isinstance(field, ForeignKeyField) else None,
+
+        for column in mapper.columns:
+            column_info = {
+                "name": column.name,
+                "code": column.name,
+                "type": column.type.__class__.__name__,
+                "size": getattr(column.type, "length", None),
+                "required": not column.nullable,
+                "relation": None,
             }
-            if hasattr(field, "choices") and field.choices:
-                field_info["options"] = [option[0] for option in field.choices]
-            description["fields"].append(field_info)
+
+            if column.foreign_keys:
+                # Aquí solo se toma el primer ForeignKey, hay que modificarlo si puede haber múltiples referencias.
+                related_model = list(column.foreign_keys)[0].column.table.name
+                column_info["relation"] = related_model
+
+            description["fields"].append(column_info)
+
+        # Verificar relaciones (como many-to-one)
+        for name, relation in mapper.relationships.items():
+            if isinstance(relation, RelationshipProperty):
+                relation_info = {
+                    "name": name,
+                    "code": name,
+                    "type": "RelationshipProperty",
+                    "size": None,
+                    "required": not relation.uselist,  # True para many-to-one, False para many-to-many
+                    "relation": relation.entity.class_.__name__,
+                }
+                description["fields"].append(relation_info)
+
         return description
