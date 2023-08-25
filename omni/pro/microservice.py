2474c0352957b96ab98f6105bfddb693597b92da
Edgar Elias Peña De La Torre <edgar.pena@omni.pro>
2023-08-25 16:00:05 -0500

    feat: add service name for saas-ms-rule

--- omni/pro/microservice.py
+++ omni/pro/microservice.py
@@ -9,6 +9,7 @@ class MicroService(Enum):
     SAAS_MS_STOCK = "saas-ms-stock"
     SAAS_MS_CLIENT = "saas-ms-client"
     SAAS_MS_SALE = "saas-ms-sale"
+    SAAS_MS_RULE = "saas-ms-rule"
 
 
 class MicroServiceDocument(object):
