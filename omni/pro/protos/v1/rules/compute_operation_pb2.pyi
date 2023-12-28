b35815e4fd04cda14d868388a3bc4abc712a1739
GitHub Action Deploy <deploy.action@omni.pro>
2023-12-28 21:32:18 +0000

    Generated protos for library deployment, Merge pull request #505 from Omnipro-Solutions/develop

--- omni/pro/protos/v1/rules/compute_operation_pb2.pyi
+++ omni/pro/protos/v1/rules/compute_operation_pb2.pyi
@@ -7,6 +7,7 @@ from typing import Union as _Union
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import struct_pb2 as _struct_pb2
+from google.protobuf import wrappers_pb2 as _wrappers_pb2
 from google.protobuf.internal import containers as _containers
 from omni.pro.protos.common import base_pb2 as _base_pb2
 
@@ -78,5 +79,53 @@ class ComputeMethodStockAvailableResponse(_message.Message):
     def __init__(
         self,
         result: _Optional[_Union[_struct_pb2.ListValue, _Mapping]] = ...,
+        response_standard: _Optional[_Union[_base_pb2.ResponseStandard, _Mapping]] = ...,
+    ) -> None: ...
+
+class ComputeDeliveryCarrierData(_message.Message):
+    __slots__ = ["address", "products", "warehouse_sql_id", "delivery_method_doc_id", "internal_transfer"]
+    ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    PRODUCTS_FIELD_NUMBER: _ClassVar[int]
+    WAREHOUSE_SQL_ID_FIELD_NUMBER: _ClassVar[int]
+    DELIVERY_METHOD_DOC_ID_FIELD_NUMBER: _ClassVar[int]
+    INTERNAL_TRANSFER_FIELD_NUMBER: _ClassVar[int]
+    address: _struct_pb2.Struct
+    products: _containers.RepeatedCompositeFieldContainer[_struct_pb2.Struct]
+    warehouse_sql_id: int
+    delivery_method_doc_id: str
+    internal_transfer: _wrappers_pb2.BoolValue
+    def __init__(
+        self,
+        address: _Optional[_Union[_struct_pb2.Struct, _Mapping]] = ...,
+        products: _Optional[_Iterable[_Union[_struct_pb2.Struct, _Mapping]]] = ...,
+        warehouse_sql_id: _Optional[int] = ...,
+        delivery_method_doc_id: _Optional[str] = ...,
+        internal_transfer: _Optional[_Union[_wrappers_pb2.BoolValue, _Mapping]] = ...,
+    ) -> None: ...
+
+class ComputeDeliveryCarrierRequest(_message.Message):
+    __slots__ = ["data", "context"]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    CONTEXT_FIELD_NUMBER: _ClassVar[int]
+    data: _containers.RepeatedCompositeFieldContainer[ComputeDeliveryCarrierData]
+    context: _base_pb2.Context
+    def __init__(
+        self,
+        data: _Optional[_Iterable[_Union[ComputeDeliveryCarrierData, _Mapping]]] = ...,
+        context: _Optional[_Union[_base_pb2.Context, _Mapping]] = ...,
+    ) -> None: ...
+
+class ComputeDeliveryCarrierResponse(_message.Message):
+    __slots__ = ["carrier_sql_id", "exceptions", "response_standard"]
+    CARRIER_SQL_ID_FIELD_NUMBER: _ClassVar[int]
+    EXCEPTIONS_FIELD_NUMBER: _ClassVar[int]
+    RESPONSE_STANDARD_FIELD_NUMBER: _ClassVar[int]
+    carrier_sql_id: int
+    exceptions: _containers.RepeatedCompositeFieldContainer[_struct_pb2.Struct]
+    response_standard: _base_pb2.ResponseStandard
+    def __init__(
+        self,
+        carrier_sql_id: _Optional[int] = ...,
+        exceptions: _Optional[_Iterable[_Union[_struct_pb2.Struct, _Mapping]]] = ...,
         response_standard: _Optional[_Union[_base_pb2.ResponseStandard, _Mapping]] = ...,
     ) -> None: ...
