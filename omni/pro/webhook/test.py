7e2cb160c3de5a86ae2599d1b142b531dcd2dd69
Janier Alejandro Banguera Correa <alejandro.banguera@omni.pro>
2024-03-05 23:20:31 +0100

    Update webhook test to send data to newrelic

--- omni/pro/webhook/test.py
+++ omni/pro/webhook/test.py
@@ -5,7 +5,7 @@ logger = configure_logger(name=__name__)
 
 
 def test_weebhook(tenant):
-    logger.info(f"Tenant: {tenant}")
+    logger.info(f"Send data to newrelic")
     register_event_new_relic(
         "TEST_WEBHOOK", {"status": "success", "message": "Webhook is working fine", "tenant": tenant}
     )
