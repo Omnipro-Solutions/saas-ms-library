621a0712cc5831316d40b406569b0b4b043cb9b7
Janier Alejandro Banguera Correa <alejandro.banguera@omni.pro>
2024-03-06 14:38:54 +0100

    Add register_event_new_relic function to omni/pro/newrelic.py

--- omni/pro/newrelic.py
+++ omni/pro/newrelic.py
@@ -5,5 +5,15 @@ logger = configure_logger(name=__name__)
 
 
 def register_event_new_relic(event_name: str, event_data: dict):
+    """
+    Registers a custom event in New Relic.
+
+    Args:
+        event_name (str): The name of the event.
+        event_data (dict): The data associated with the event.
+
+    Returns:
+        None
+    """
     agent.record_custom_event(event_name, event_data)
     logger.info(f"Register: {event_name}, in newrelic")
