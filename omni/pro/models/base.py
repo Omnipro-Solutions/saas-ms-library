825be4722843bbb51e92a45700c071362d373109
Edgar Elias Peña De La Torre <edgar.pena@omni.pro>
2024-04-30 19:43:10 -0500

    Refactor BaseDocument post_save method to send different actions to Airflow based on whether the document is created or updated

--- omni/pro/models/base.py
+++ omni/pro/models/base.py
@@ -136,18 +136,18 @@ class BaseDocument(Document):
     @classmethod
     def post_save(cls, sender, document, **kwargs):
         # identify if the object is a new instance or an existing one
-        if document._changed_fields and not document._created:
-            ActionToAirflow.send_to_airflow(
-                cls,
-                document,
-                action="update",
-                context={"tenant": document.context.tenant, "user": document.context.user},
-            )
-        elif document._created:
+        if kwargs.get("created", False):
             ActionToAirflow.send_to_airflow(
                 cls,
                 document,
                 action="create",
+                context={"tenant": document.context.tenant, "user": document.context.user},
+            )
+        elif document._changed_fields:
+            ActionToAirflow.send_to_airflow(
+                cls,
+                document,
+                action="update",
                 context={"tenant": document.context.tenant, "user": document.context.user},
             )
 
