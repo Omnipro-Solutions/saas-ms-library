5e514c254bb86ffdb382179a4fdc0b8b8394b979
Julián Valdés <julianvaldes24@gmail.com>
2024-05-09 08:20:13 -0500

    chore: update dependencies in setup.py
    define versions requirements versions
    
    boto3==1.34.101
    mongoengine==0.27.0
    marshmallow==3.21.2
    SQLAlchemy==2.0.24
    sqlalchemy-utils==0.41.1
    networkx==3.1
    unidecode==1.3.8
    omni-pro-base>=0.0.0,<=2.0.0
    omni-pro-redis>=0.0.0,<=2.0.0
    omni-pro-grpc>=0.0.0,<=2.0.0
    apache-airflow-client==2.8.0
    newrelic==8.8.1
    alembic==1.11.2
    blinker==1.7.0

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ AUTHOR = "OMNI.PRO"
 AUTHOR_EMAIL = "development@omni.pro"
 URL = "https://github.com/Omnipro-Solutions/saas-ms-library"
 INSTALL_REQUIRES = [
-    "boto3>=1.20.0 , <=1.50.0",
-    "mongoengine>=0.20.0,<=0.27.0",
-    "marshmallow>=3.0.0,<4.0.0",
-    "SQLAlchemy>=1.3.0,<3.0.0",
+    "boto3==1.34.101",
+    "mongoengine==0.27.0",
+    "marshmallow==3.21.2",
+    "SQLAlchemy==2.0.24",
     "sqlalchemy-utils==0.41.1",
-    "networkx>=2.0, <=4.0",
-    "unidecode>=1.0,<=3.0",
+    "networkx==3.1",
+    "unidecode==1.3.8",
     "omni-pro-base>=0.0.0,<=2.0.0",
     "omni-pro-redis>=0.0.0,<=2.0.0",
     "omni-pro-grpc>=0.0.0,<=2.0.0",
